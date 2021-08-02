b33498a9d26b20c4148407a23141c509eb694385
Dane Edwards <Daneedw@gmail.com>
2021-08-02 08:39:23 -0500

    basic practice initializing repo

---
+++ bool/index.js
@@ -0,0 +1,24 @@
+//create a boolean that contains the value false. Give it a name of isHappy.
+
+//create a boolean that contains the value false. Give it a name of isDeveloper.
+
+//create a boolean that contains the value true. Give it a name of isOverwhelmed.
+
+//create a boolean that contains the value true. Give it a name of isAngry.
+
+
+//set isHappy to true.
+
+//set isDeveloper to true.
+
+//set isOverwhelmed to false.
+
+//set isAngry to false.
+
+
+//logical and all of the values into a separate value called isHappyAndDeveloper
+//(i.e isHappy && isDeveloper)
+
+
+
+
---
+++ string/index.js
@@ -0,0 +1,8 @@
+//store your name in a string called name using double quotes.
+
+//store a string containing the value is happy using double quotes!
+
+//concatenate the two values into a separate variable called sentence.
+
+//now do the above but with single quotes.
+
